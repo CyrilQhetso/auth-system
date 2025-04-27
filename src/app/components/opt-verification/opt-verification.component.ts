b3c3b47de105e7ba69bb7db62441524277bfe77b
CyrilQhetso <cyril.qhetso@gmail.com>
2025-04-27 11:35:04 +0200

    auth-system structure

---
+++ src/app/components/opt-verification/opt-verification.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opt-verification',
+  standalone: false,
+  templateUrl: './opt-verification.component.html',
+  styleUrl: './opt-verification.component.css'
+})
+export class OptVerificationComponent {
+
+}
